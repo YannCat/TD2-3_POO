22ae4c208fd576550a97373e6c5890feb02ec70f
Yann CATALA <yann@plusdebornes.fr>
2019-01-19 11:39:38 +0100

    Add some functions

--- Point.hpp
+++ Point.hpp
@@ -1,4 +1,9 @@
+#include <string>	
+
 struct Point
 {
 	int xpos, ypos;	
-}
+};
+
+Point InitPoint(int x, int y);
+std::string Display(Point lepoint);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include "Point.hpp"
+
 using namespace std;
 
 int main(int argc, char **argv){
 	
 }
+
+Point InitPoint(int x, int y)
+{
+	Point monPoint;
+
+	monPoint.xpos = x;
+	monPoint.ypos = y;
+	
+	return monPoint;
+}
+	
+std::string Display(Point lepoint)
+{
+	string point;
+	point = "(" + to_string(lepoint.xpos) + ", " + to_string(lepoint.ypos) + ")";
+	return point;
+}
+
