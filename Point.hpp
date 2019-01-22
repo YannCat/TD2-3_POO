01cb75a8379844707ff36580155c1517c4cb71b3
Yann CATALA <yann@plusdebornes.fr>
2019-01-22 10:45:21 +0100

    Add Catch framework

--- Point.hpp
+++ Point.hpp
@@ -1,4 +1,7 @@
-#include <string>	
+//#define CATCH_CONFIG_MAIN
+//#include "catch.hpp"
+#include <string>
+#include <iostream>
 
 struct Point
 {
@@ -7,3 +10,5 @@ struct Point
 
 Point InitPoint(int x, int y);
 std::string Display(Point lepoint);
+Point additionneur (Point point1, Point point2);
+Point operation (Point lepoint);
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include <iostream>
-#include <string>
 #include "Point.hpp"
 
 using namespace std;
 
 int main(int argc, char **argv){
 	
+	Point monpoint = InitPoint(1, 2);
+	
+	monpoint = operation(monpoint);
+	string affiche = Display(monpoint);
+	cout<<affiche<<endl;
+	return 0;
 }
 
 Point InitPoint(int x, int y)
@@ -25,3 +29,39 @@ std::string Display(Point lepoint)
 	return point;
 }
 
+Point additionneur (Point point1, Point point2)
+{
+	Point resultat;
+	resultat.xpos =point1.xpos + point2.xpos;
+	resultat.ypos =point1.ypos + point2.ypos;
+	return resultat;
+}
+
+Point operation ( Point lepoint)
+{
+	string c;	
+	cout<<"Entrez l'opération à executer :"<<endl<<"p++ : incrémente x de 1"<<endl<<"++p : incrémente y de 1"<<endl<<"p-- : décrémente x de 1"<<endl<<"--p : décrémente x de 1"<<endl;
+	cin>>c;
+	
+	if(c=="p++")
+		{
+		lepoint.xpos++;
+		return lepoint;
+		}
+	else if(c=="++p")
+		{ 
+		lepoint.ypos++;
+		return lepoint;
+		}
+	else if(c=="p--") 
+		{
+		lepoint.xpos--;
+		return lepoint;
+		}
+	else if(c=="--p") 
+		{
+		lepoint.ypos--;
+		return lepoint;
+		}
+	else return lepoint;
+}
