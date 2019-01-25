8e962135bbd16c1b152742827d1c09d76440f522
Yann CATALA <yann@plusdebornes.fr>
2019-01-25 08:38:05 +0100

    Fini !

--- Makefile
+++ Makefile
@@ -1,22 +1,22 @@
 CC=g++
 CFLAGS=-Wall -g -O0 -std=c++11
-TARGET_NAME=Point
-TARGET_TEST=test-point
+TARGET_NAME=Point.exe
+TARGET_TEST=test-point.exe
 SRC=src/Point.cpp src/main.cpp
 SRC_TEST=src/Point.cpp src/PointTest.cpp
 DEPS=-I headers/
 
-default: all
+default: run 
 
-all:
+point:
 	$(CC) -o $(TARGET_NAME) $(SRC) $(DEPS) $(CFLAGS)
 
 test : 
 	$(CC) -o $(TARGET_TEST) $(SRC_TEST) $(DEPS) $(CFLAGS)
-	./test-point
+	./$(TARGET_TEST)
 
 clean:
-	rm $(TARGET_NAME) $(TARGET_TEST)
+	rm *.exe 
 
-run:	all
-	./Point
+run:	point
+	./$(TARGET_NAME)
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,31 @@
 
 int main(int argc, char **argv){
 	
-	Point monpoint;
+	Point A, B, D;
 	int x , y;
 	string c;
 	cout<<"Entrez les coordonnées du point A :"<<endl<<"Pour x : ";
 	cin>>x;
 	cout<<"Pour Y : ";
 	cin>>y;
-	monpoint = InitPoint(x, y);
+	A = InitPoint(x, y);
+	string affiche = Display(A);
+	cout<<endl<<"Coordonnées du point A : "<<affiche<<endl<<endl;
 	
-	string affiche = Display(monpoint);
-	cout<<endl<<"Coordonnées du point A : "<<affiche<<endl;
+	cout<<"Entrez les coordonnées du point B :"<<endl<<"Pour x : ";
+	cin>>x;
+	cout<<"Pour Y : ";
+	cin>>y;
+	B = InitPoint(x, y);
+	affiche = Display(B);
+	cout<<endl<<"Coordonnées du point B : "<<affiche<<endl<<endl;
+		
+	D = additionneur(A,B);
+	cout<<"L'addition de ces 2 points retourne : "<<Display(D)<<endl<<endl;
+	
 	cout<<"Entrez l'opération à executer :"<<endl<<"p++ : incrémente x de 1"<<endl<<"++p : incrémente y de 1"<<endl<<"p-- : décrémente x de 1"<<endl<<"--p : décrémente x de 1"<<endl;
 	cin>>c;
-	monpoint = operation (monpoint, c);
-	cout<<"Nouvelle coordonnées : "<<Display(monpoint)<<endl;
+	A = operation (A, c);
+	cout<<"Nouvelle coordonnées : "<<Display(A)<<endl;
 	return 0;
 }
